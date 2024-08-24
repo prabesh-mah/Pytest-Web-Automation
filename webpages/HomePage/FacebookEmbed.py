b9b538e737b223c491a5035094d552f04cdf88b6
WickedMan <wickedman9991@gmail.com>
2024-08-25 02:56:07 +0545

    update as locator changed

--- webpages/HomePage/FacebookEmbed.py
+++ webpages/HomePage/FacebookEmbed.py
@@ -1,4 +1,5 @@
 from webpages.BasePage import BasePage
+from webpages.BaseLocators import HomepageLocator
 from selenium.webdriver.common.by import By
 import time
 
@@ -10,8 +11,6 @@ class FacebookEmbedLocator:
     close_button = (By.XPATH, "//div[@aria-label='Close']")
     element_to_move = (
         By.XPATH, "//h4[@class='box_header' and contains(text(),'Nepse Alpha Facebook Page')]")
-    homepage_element = (
-        By.XPATH, "//h4[@class='box_header' and text()='About Nepse Alpha']")
 
 
 class FacebookEmbedSection(BasePage):
@@ -28,4 +27,4 @@ class FacebookEmbedSection(BasePage):
         self.click(FacebookEmbedLocator.close_button)
 
     def homepage_element_is_present(self):
-        self.check_element(FacebookEmbedLocator.homepage_element)
+        self.check_element(HomepageLocator.nepse_alpha_fb_page)
