4d8c8de3eca42f573c8acf6d00858ead7eb371f7
WickedMan <wickedman9991@gmail.com>
2024-08-27 11:51:48 +0545

    added browser options

--- conftest.py
+++ conftest.py
@@ -15,27 +15,38 @@ def browser_call(request):
     baseURL = ReadConfig.read_configuration("config file", "base_url")
     browserType = ReadConfig.read_configuration("config file", "browser_type")
 
-    browser_options = uc.ChromeOptions()
+    chromium_options = uc.ChromeOptions()
     extension_paths = [
         "extensions/Buster",
         "extensions/uBlock Origin"
     ]
-    browser_options.add_argument(
+    chromium_options.add_argument(
         f"--load-extension={','.join(extension_paths)}")
-    browser_options.add_argument(
+    chromium_options.add_argument(
         "--disable-blink-features=AutomationControlled")
-    browser_options.add_argument("--disable-extensions")
+    # chromium_options.add_argument("--headless")
+    chromium_options.add_argument("--disable-infobars")
+    chromium_options.add_argument("--disable-gpu")
+    chromium_options.add_argument("--ignore-certificate-errors")
+    chromium_options.add_argument("--disable-extensions")
+
+    firefox_options = webdriver.FirefoxOptions()
+    # firefox_options.headless = True
+    firefox_options.set_preference(
+        "browser.startup.homepage_override.mstone", "ignore")
+    firefox_options.set_preference("security.ssl.enable_ocsp_stapling", False)
+    firefox_options.set_preference("app.update.enabled", False)
 
     # Initialize Webdriver
     driver = None
 
     try:
         if browserType == "chrome":
-            driver = uc.Chrome(options=browser_options)
+            driver = uc.Chrome(options=chromium_options)
         elif browserType == "firefox":
-            driver = webdriver.Firefox(options=browser_options)
+            driver = webdriver.Firefox(options=firefox_options)
         elif browserType == "edge":
-            driver = webdriver.Edge(options=browser_options)
+            driver = webdriver.Edge(options=chromium_options)
         else:
             pytest.skip("Unsupported browser!")
 
@@ -46,6 +57,11 @@ def browser_call(request):
         # Maximize window and open the base URL
         driver.maximize_window()
         driver.get(baseURL)
+
+        # Wait for a specific element to be visible after the page loads
+        WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
+            (By.XPATH, "//img[@src='/images/logo.jpg']")))
+
         assert "nepsealpha" in driver.current_url
 
         # Sharing WebDriver instance across all test methods in a class
