6d542437714b2c8d86fe344b4bb9d63d5891fcc4
WickedMan <wickedman9991@gmail.com>
2024-09-07 11:43:27 +0545

    removed unnecessary lines

--- tests/HomePageTest/copyright_test.py
+++ tests/HomePageTest/copyright_test.py
@@ -7,29 +7,13 @@ import time
 
 @pytest.mark.smoke
 @pytest.mark.regression
-class TestCopyright(BaseTest):
+class TestCopyrightLink(BaseTest):
 
     @pytest.mark.order(21)
     @pytest.mark.timeout(30)
     @pytest.mark.high_priority
     def test_verify_copyright_text_and_symbol(self):
         copyright = CopyrightSection(self.driver)
-
-        # expected_company_name = "Nepse Alpha Pvt Ltd."
-        # expected_symbol = "©"
-
-        # company_name_matches = copyright.copyright_company_name_text(
-        #     expected_company_name)
-        # print(f"The copyright text matches the expected company name: {
-        #     expected_company_name}")
-        # assert company_name_matches, f"The copyright text does not match the expected company name: {
-        #     expected_company_name}"
-
-        # symbol_matches = copyright.copyright_symbol(expected_symbol)
-        # print(f"The copyright symbol matches the expected symbol: {
-        #     expected_symbol}")
-        # assert symbol_matches, f"The copyright symbol does not match the expected symbol: {
-        #     expected_symbol}"
 
         expected_name = '© Copyright Nepse Alpha Pvt Ltd.'
 
