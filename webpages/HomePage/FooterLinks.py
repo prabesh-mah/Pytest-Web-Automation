4095e52aed694fcfdf1e2323159974796e001bec
WickedMan <wickedman9991@gmail.com>
2024-12-29 21:14:27 +0545

    optimized code

--- webpages/HomePage/FooterLinks.py
+++ webpages/HomePage/FooterLinks.py
@@ -2,6 +2,8 @@ from webpages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import requests
+import time
+import random
 
 
 class FooterLinksLocator:
@@ -12,20 +14,23 @@ class FooterLinkSection(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    def verify_all_footer_links(self):
+    def validate_footer_links(self):
         footer_links = self.check_elements(FooterLinksLocator.footer_links)
 
         for link in footer_links:
-            url = link.get_attribute("href")  # get href from HTML Tag
+            url = link.get_attribute("href")
 
             try:
-                # Send HTTP HEAD request to all URLs
                 response = requests.head(url, timeout=10)
-                if response.status_code >= 400:  # Check for broken links
+                if response.status_code != 200:
                     print(f"Broken link: {
                           url} - Status code: {response.status_code}")
+                    raise AssertionError(f"Link {url} is broken with status code: {
+                                         response.status_code}")
                 else:
                     print(
                         f"Link {url} is working fine - Status code: {response.status_code}")
             except requests.exceptions.RequestException as e:
                 print(f"Error accessing {url}: {e}")
+                raise AssertionError(f"Error accessing {url}: {e}")
+            time.sleep(random.uniform(1, 3))
